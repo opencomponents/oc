7dc3e45101b6efe87ba62e01354a66959aba1361
Matteo Figus <matteofigus@gmail.com>
2015-02-16 15:37:59 +0000

    ie8 failover div rendering

--- components/oc-client/src/oc-client.js
+++ components/oc-client/src/oc-client.js
@@ -2,11 +2,10 @@
 
 var oc = oc || {};
 
-(function(Handlebars, $document, $window, debug){
+(function(Handlebars, head, $document, $window, debug){
 
   // Constants
-  var HTML5SHIV_URL = 'https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js',
-      IE89_AJAX_POLYFILL_URL = 'https://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxtransport-xdomainrequest/1.0.3/jquery.xdomainrequest.min.js',
+  var IE89_AJAX_POLYFILL_URL = 'https://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxtransport-xdomainrequest/1.0.3/jquery.xdomainrequest.min.js',
       JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.js',
       RETRY_INTERVAL = 5000,
       OC_TAG = 'oc-component',
@@ -22,7 +21,13 @@ var oc = oc || {};
   // The code
   var headScripts = [],
       $ = typeof(jQuery) !== 'undefined' ? jQuery : undefined,
-      noop = function(){};
+      noop = function(){},
+      nav = $window.navigator.userAgent,
+      is8 = !!(nav.match(/MSIE 8/)),
+      is9 = !!(nav.match(/MSIE 9/)),
+      initialised = false,
+      initialising = false,
+      readySubscribers = [];
 
   var logger = {
     error: function(msg){
@@ -44,39 +49,6 @@ var oc = oc || {};
     return -1;
   };
 
-  // a minimal vanilla require.js
-  var _require = function(hrefs, callback){
-
-    var callbacks = hrefs.length,
-        $head = $document.getElementsByTagName('head')[0];
-
-    var tryExit = function(){ 
-      callbacks--;
-      if(callbacks === 0 && typeof(callback) === 'function'){ 
-        callback();
-        callback = noop;
-      }
-    };
-
-    var appendScript = function(href){
-      headScripts.push(href);
-      var $script = $document.createElement('script');
-
-      $script.type = 'text/javascript';
-      $script.src = href;
-      $script.onload = tryExit;
-      $head.appendChild($script);
-    };
-
-    for(var i = 0; i < hrefs.length; i++){
-      if(_indexOf(headScripts, hrefs[i]) < 0){
-        appendScript(hrefs[i]);
-      } else {
-        tryExit();
-      }
-    }
-  };
-
   var processHtml = function($component, data, callback){
 
     var newId = Math.floor(Math.random()*9999999999);
@@ -94,147 +66,182 @@ var oc = oc || {};
     callback();
   };
 
-  oc.render = function(compiledView, model, callback){
-    if(!oc.components[compiledView.key]){
-      callback(MESSAGES_ERRORS_LOADING_COMPONENT.replace('{0}', compiledView.key));
-    } else {
-      var compiledComponent = '';
+  oc.ready = function(callback){
 
-      if(compiledView.type === 'handlebars'){
-        var linkedComponent = Handlebars.template(oc.components[compiledView.key], []);
-        compiledComponent = linkedComponent(model);
-      } else if(compiledView.type === 'jade'){
-        compiledComponent = oc.components[compiledView.key](model);
+    if(initialised){
+      return callback();
+    } else if(initialising) {
+      readySubscribers.push(callback);
+    } else {
+
+      initialising = true;
+
+      var requirePolyfills = function(cb){
+        if(is8 || is9){
+          head.load(IE89_AJAX_POLYFILL_URL, cb);
+        } else {
+          cb();
+        }
+      };
+
+      var done = function(){
+        initialised = true;
+        initialising = false;
+        callback();
+        for(var i = 0; i < readySubscribers.length; i++){
+          readySubscribers[i]();
+        }
+      };
+
+      if(!$){
+        head.load(JQUERY_URL, function(){
+          $ = jQuery;
+          requirePolyfills(done);
+        });
       } else {
-        return callback(MESSAGES_ERRORS_VIEW_ENGINE_NOT_SUPPORTED.replace('{0}', compiledView.type));
+        requirePolyfills(done);
       }
-      callback(null, compiledComponent);
     }
   };
 
-  oc.renderNestedComponent = function($component, callback){
-    $component.html('<div class="oc-loading">' + MESSAGES_LOADING_COMPONENT + '</div>');
+  oc.render = function(compiledView, model, callback){
+    oc.ready(function(){
+      if(!oc.components[compiledView.key]){
+        callback(MESSAGES_ERRORS_LOADING_COMPONENT.replace('{0}', compiledView.key));
+      } else {
+        var compiledComponent = '';
 
-    oc.renderByHref($component.attr('href'), function(err, data){
-      if(err || !data || !data.html){
-        return logger.error(err);
+        if(compiledView.type === 'handlebars'){
+          var linkedComponent = Handlebars.template(oc.components[compiledView.key], []);
+          compiledComponent = linkedComponent(model);
+        } else if(compiledView.type === 'jade'){
+          compiledComponent = oc.components[compiledView.key](model);
+        } else {
+          return callback(MESSAGES_ERRORS_VIEW_ENGINE_NOT_SUPPORTED.replace('{0}', compiledView.type));
+        }
+        callback(null, compiledComponent);
       }
+    });
+  };
 
-      processHtml($component, data, callback);
+  oc.renderNestedComponent = function($component, callback){
+    oc.ready(function(){
+      $component.html('<div class="oc-loading">' + MESSAGES_LOADING_COMPONENT + '</div>');
+
+      oc.renderByHref($component.attr('href'), function(err, data){
+        if(err || !data || !data.html){
+          return logger.error(err);
+        }
+
+        processHtml($component, data, callback);
+      });
     });
   };
 
   oc.renderByHref = function(href, callback){
-    if(href !== ''){
-      $.ajax({
-        url: href,
-        headers: { 'render-mode': 'pre-rendered' }, 
-        crossDomain: true,
-        async: true,
-        success: function(apiResponse){
-          if(apiResponse.renderMode === 'pre-rendered'){
-            _require([apiResponse.template.src], function(){
-              oc.render(apiResponse.template, apiResponse.data, function(err, html){
-                if(err){ 
-                  return callback(MESSAGES_ERRORS_RENDERING.replace('{0}', apiResponse.href).replace('{1}', err));
-                }
-                logger.info(MESSAGES_RENDERED.replace('{0}', apiResponse.template.src));
-                callback(null, {
-                  html: html, 
-                  key: apiResponse.template.key,
-                  version: apiResponse.version
+    oc.ready(function(){
+      if(href !== ''){
+        $.ajax({
+          url: href,
+          headers: { 'render-mode': 'pre-rendered' }, 
+          crossDomain: true,
+          async: true,
+          success: function(apiResponse){
+            if(apiResponse.renderMode === 'pre-rendered'){
+              head.load([apiResponse.template.src], function(){
+                oc.render(apiResponse.template, apiResponse.data, function(err, html){
+                  if(err){ 
+                    return callback(MESSAGES_ERRORS_RENDERING.replace('{0}', apiResponse.href).replace('{1}', err));
+                  }
+                  logger.info(MESSAGES_RENDERED.replace('{0}', apiResponse.template.src));
+                  callback(null, {
+                    html: html, 
+                    key: apiResponse.template.key,
+                    version: apiResponse.version
+                  });
                 });
               });
-            });
-          } else if(apiResponse.renderMode === 'rendered'){
-            logger.info(MESSAGES_RENDERED.replace('{0}', apiResponse.href));
-            callback(null, {
-              html: $(apiResponse.html).html(), 
-              version: apiResponse.version
-            });            
+            } else if(apiResponse.renderMode === 'rendered'){
+              logger.info(MESSAGES_RENDERED.replace('{0}', apiResponse.href));
+
+              var innerHtmlPlusEnding = apiResponse.html.slice(apiResponse.html.indexOf('>') + 1),
+                  innerHtml = innerHtmlPlusEnding.slice(0, innerHtmlPlusEnding.indexOf('<'));
+
+              callback(null, {
+                html: innerHtml, 
+                version: apiResponse.version
+              });            
+            }
+          },
+          error: function(){
+            logger.error(MESSAGES_ERRORS_RETRIEVING);
+            setTimeout(function() {
+              oc.renderByHref(href, callback);
+            }, RETRY_INTERVAL);
           }
-        },
-        error: function(){
-          logger.error(MESSAGES_ERRORS_RETRIEVING);
-          setTimeout(function() {
-            oc.renderByHref(href, callback);
-          }, RETRY_INTERVAL);
-        }
-      });
-    } else {
-      return callback(MESSAGES_ERRORS_RENDERING.replace('{1}', MESSAGES_ERRORS_HREF_MISSING));
-    }
+        });
+      } else {
+        return callback(MESSAGES_ERRORS_RENDERING.replace('{1}', MESSAGES_ERRORS_HREF_MISSING));
+      }
+    });
   };
 
-  oc.renderUnloadedComponents = oc.refresh = function(){
-    var $unloadedComponents = $(OC_TAG + '[data-rendered!=true]');
+  oc.renderUnloadedComponents = function(){
+    oc.ready(function(){
+      var selector = (is8 ? 'div[data-oc-component=true]' : OC_TAG),
+          $unloadedComponents = $(selector + '[data-rendered!=true]');
 
-    var renderUnloadedComponent = function($unloadedComponents, i){
-      logger.info(MESSAGES_RETRIEVING);
-      oc.renderNestedComponent($($unloadedComponents[i]), function(){
-        i++;
-        if(i < $unloadedComponents.length){
-          renderUnloadedComponent($unloadedComponents, i);
-        } else {
-          oc.renderUnloadedComponents();
-        }
-      });
-    };
+      var renderUnloadedComponent = function($unloadedComponents, i){
+        logger.info(MESSAGES_RETRIEVING);
+        oc.renderNestedComponent($($unloadedComponents[i]), function(){
+          i++;
+          if(i < $unloadedComponents.length){
+            renderUnloadedComponent($unloadedComponents, i);
+          } else {
+            oc.renderUnloadedComponents();
+          }
+        });
+      };
 
-    if($unloadedComponents.length > 0){
-      renderUnloadedComponent($unloadedComponents, 0);
-    }
+      if($unloadedComponents.length > 0){
+        renderUnloadedComponent($unloadedComponents, 0);
+      }
+    });
   };
 
   oc.setEventListeners = function(component){
-    component.off('reRender');
-    component.on('reRender', function(event, href){
-      var self = $(event.target);
-      if(!!href && href !== ''){
-        self.attr('href', href);
-      }
-      self.attr('data-hash', '');
-      self.attr('data-rendered', false);
-      oc.renderUnloadedComponents();
-      return false;
+    oc.ready(function(){
+      component.off('reRender');
+      component.on('reRender', function(event, href){
+        var self = $(event.target);
+        if(!!href && href !== ''){
+          self.attr('href', href);
+        }
+        self.attr('data-hash', '');
+        self.attr('data-rendered', false);
+        oc.renderUnloadedComponents();
+        return false;
+      });
+      component.trigger('loaded');
     });
-    component.trigger('loaded');
   };
 
   oc.load = function(placeholder, href, callback){
-    if(typeof(callback) !== 'function'){
-      callback = noop;
-    }
+    oc.ready(function(){
+      if(typeof(callback) !== 'function'){
+        callback = noop;
+      }
 
-    if($(placeholder)){
-      $(placeholder).html('<' + OC_TAG + ' href="' + href + '" />');
-      var newComponent = $(OC_TAG, placeholder);
-      oc.renderNestedComponent(newComponent, function(){
-        callback(newComponent);
-      });
-    }
+      if($(placeholder)){
+        $(placeholder).html('<' + OC_TAG + ' href="' + href + '" />');
+        var newComponent = $(OC_TAG, placeholder);
+        oc.renderNestedComponent(newComponent, function(){
+          callback(newComponent);
+        });
+      }
+    });
   };
 
-  var ensureJqueryIsLoaded = function(callback){
-    if(!$){
-      _require([JQUERY_URL], function(){
-        $ = jQuery;
+  oc.ready(oc.renderUnloadedComponents);
 
-        var nav = $window.navigator.userAgent,
-            is8 = !!(nav.match(/MSIE 8/)),
-            is9 = !!(nav.match(/MSIE 9/));
-
-        if(is8 || is9){
-          _require([IE89_AJAX_POLYFILL_URL, HTML5SHIV_URL], callback);
-        } else {
-          callback();
-        }
-      });
-    } else {
-      callback();
-    }
-  };
-
-  ensureJqueryIsLoaded(oc.renderUnloadedComponents);
-
-})(Handlebars, document, window, true); // jshint ignore:line
+})(Handlebars, head, document, window, true); // jshint ignore:line
