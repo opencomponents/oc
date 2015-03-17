a5a9cee5e54001d7ecca11bea308c8822e7bc330
Matteo Figus <matteofigus@gmail.com>
2015-03-17 18:11:20 +0000

    karma

---
+++ grunt-tasks/karma.js
@@ -0,0 +1,12 @@
+module.exports = {
+    options: {
+        configFile: 'test/karma.conf.js'
+    },
+    dev: {
+        singleRun: false,
+        autoWatch: true
+    },
+    test: {
+        singleRun: true
+    }
+};
---
+++ test/karma.conf.js
@@ -0,0 +1,30 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', 'sinon'],
+    files: [
+      '../components/oc-client/src/head.load.js',
+      '../node_modules/handlebars/dist/handlebars.runtime.min.js',
+      '../node_modules/jade/runtime.js',
+      '../components/oc-client/src/oc-client.js',
+
+      'front-end/*.js'
+    ],
+    reporters: ['dots'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
+    singleRun: false
+  });
+};
