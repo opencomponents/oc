65f55fe33e565c8184040fd90af460c4c6a2f3e6
Matteo Figus <matteofigus@gmail.com>
2015-03-17 12:31:21 +0000

    Fixing console verbosity

--- registry/app-start.js
+++ registry/app-start.js
@@ -13,33 +13,35 @@ module.exports = function(repository, options, callback){
     return callback(null, 'ok');
   }
 
-  console.log(format(colors.yellow('Connecting to library: {0}/{1}'), options.s3.bucket, options.s3.componentsDir));
+  var logger = !!options.verbosity ? console : { log: _.noop };
+
+  logger.log(format(colors.yellow('Connecting to library: {0}/{1}'), options.s3.bucket, options.s3.componentsDir));
 
   repository.getComponentVersions('oc-client', function(err, componentInfo){
 
     if(err){
-      return console.log(colors.red(err));
+      return logger.log(colors.red(err));
     }
 
-    console.log(format(colors.yellow('Ensuring oc-client@{0} is available on library...'), packageInfo.version));
+    logger.log(format(colors.yellow('Ensuring oc-client@{0} is available on library...'), packageInfo.version));
 
     if(!_.contains(componentInfo, packageInfo.version)){
 
-      console.log(colors.yellow('Component not found. Publishing it...'));
+      logger.log(colors.yellow('Component not found. Publishing it...'));
 
       var componentPath = path.resolve(__dirname, '../components/oc-client/_package');
       
       repository.publishComponent(componentPath, 'oc-client', packageInfo.version, function(err, res){
         if(!err){
-          console.log(colors.green('Component published.'));
+          logger.log(colors.green('Component published.'));
         } else {
-          console.log(colors.red(format('Component not published: {0}', _.first(err).message)));
+          logger.log(colors.red(format('Component not published: {0}', _.first(err).message)));
         }
 
         callback(err, res);
       });
     } else {
-      console.log(colors.green('Component is available on library.'));
+      logger.log(colors.green('Component is available on library.'));
       callback(null, 'ok');
     }
   });
