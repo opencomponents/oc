73a6cc20b6b12f6060721b375c12f62a36facb49
Matteo Figus <matteofigus@gmail.com>
2015-03-17 02:45:44 +0000

    Added the container options on package to avoid container to be shown on server-side rendering

---
+++ test/fixtures/components/no-container/_package/template.js
@@ -0,0 +1,1 @@
+var oc=oc||{};oc.components=oc.components||{},oc.components["18e2619ff1d06451883f21656affd4c6f02b1ed1"]=function(o,e,c,n,f){return this.compilerInfo=[4,">= 1.0.0"],c=this.merge(c,o.helpers),f=f||{},"Hello world!"};
