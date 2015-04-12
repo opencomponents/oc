b74631b530d736314505ef08ab3c1b6840626cdc
Matteo Figus <matteofigus@gmail.com>
2015-04-12 18:37:51 +0100

    Cleanup

--- test/configuration/karma-sauce.js
+++ test/configuration/karma-sauce.js
@@ -31,12 +31,9 @@ module.exports = function(config) {
 
   if (process.env.TRAVIS)
   {
-    //config.captureTimeout = 0;
-    config.logLevel = config.LOG_DEBUG;
+    //config.logLevel = config.LOG_DEBUG;
     config.transports = ['xhr-polling'];
     config.sauceLabs.build = 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')';
-    config.sauceLabs.startConnect = true;
-    //config.sauceLabs.recordScreenshots = true;
     config.sauceLabs.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
   }
 };
