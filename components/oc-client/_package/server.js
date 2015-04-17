820c9cbe093d4353f42afcbcb61eedf5ed77bf14
Matteo Figus <matteofigus@gmail.com>
2015-04-17 11:37:57 +0100

    0.14.2

--- components/oc-client/_package/server.js
+++ components/oc-client/_package/server.js
@@ -1,5 +1,1 @@
-'use strict';
-
-module.exports.data = function(req, callback){
-  callback(null, { staticPath: req.staticPath });
-};
+"use strict";module.exports.data=function(t,a){a(null,{staticPath:t.staticPath})};
