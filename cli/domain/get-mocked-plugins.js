98f93efe9d114d40b9d87cff5ca1e7b0355becbf
Andy Royle <ajroyle@gmail.com>
2015-10-23 17:29:20 +0100

    allow dynamic mocks for plugins

--- cli/domain/get-mocked-plugins.js
+++ cli/domain/get-mocked-plugins.js
@@ -3,32 +3,66 @@
 var fs = require('fs-extra');
 var path = require('path');
 var _ = require('underscore');
-
+var strings = require('../../resources/');
 var settings = require('../../resources/settings');
 
-module.exports = function(){
-  var mockedPlugins = [],
+var registerStaticMocks = function(mocks, logger){
+    return _.map(mocks, function(mockedValue, pluginName){
+      logger.log('├── '.green + pluginName + ' () => ' + mockedValue);
+      return {
+        name: pluginName,
+        register: {
+          register: function(options, next){
+            return next();
+          },
+          execute: function(){
+            return mockedValue;
+          }
+        }
+      };
+    });
+};
+
+var registerDynamicMocks = function(mocks, logger){
+    return _.map(mocks, function(source, pluginName){
+      var p;
+      try {
+        p = require(path.resolve(source));
+      } catch(er) {
+        logger.log(er.toString().red);
+        return;
+      }
+
+      logger.log('├── '.green + pluginName + ' () => [Function]');
+      return {
+        name: pluginName,
+        register: {
+          register: function(options, next){
+            return next();
+          },
+          execute: p
+        }
+      };
+    });
+};
+
+module.exports = function(logger){
+  var plugins = [],
       ocJsonPath = path.resolve(settings.configFile.src);
 
-  if(fs.existsSync(ocJsonPath)){
-    var content = fs.readJsonSync(ocJsonPath);
-
-    if(!!content.mocks && !!content.mocks.plugins && !!content.mocks.plugins.static){
-      _.forEach(content.mocks.plugins.static, function(mockedValue, pluginName){
-        mockedPlugins.push({
-          name: pluginName,
-          register: {
-            register: function(options, next){
-              return next();
-            },
-            execute: function(){
-              return mockedValue;
-            }
-          }
-        });
-      });
-    }
+  if(!fs.existsSync(ocJsonPath)){
+    return plugins;
   }
 
-  return mockedPlugins;
+  var content = fs.readJsonSync(ocJsonPath);
+  if(!content.mocks || !content.mocks.plugins){
+    return plugins;
+  }
+
+  logger.log(strings.messages.cli.REGISTERING_MOCKED_PLUGINS.yellow);
+
+  plugins = plugins.concat(registerStaticMocks(content.mocks.plugins.static, logger));
+  plugins = plugins.concat(registerDynamicMocks(content.mocks.plugins.dynamic, logger));
+
+  return plugins;
 };
