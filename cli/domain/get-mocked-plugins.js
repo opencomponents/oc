590e3737eced69466e45d3e723620cf83907e0cc
Andy Royle <ajroyle@gmail.com>
2015-10-23 17:33:09 +0100

    remove silly four-space tabs

--- cli/domain/get-mocked-plugins.js
+++ cli/domain/get-mocked-plugins.js
@@ -7,43 +7,43 @@ var strings = require('../../resources/');
 var settings = require('../../resources/settings');
 
 var registerStaticMocks = function(mocks, logger){
-    return _.map(mocks, function(mockedValue, pluginName){
-      logger.log('├── '.green + pluginName + ' () => ' + mockedValue);
-      return {
-        name: pluginName,
-        register: {
-          register: function(options, next){
-            return next();
-          },
-          execute: function(){
-            return mockedValue;
-          }
+  return _.map(mocks, function(mockedValue, pluginName){
+    logger.log('├── '.green + pluginName + ' () => ' + mockedValue);
+    return {
+      name: pluginName,
+      register: {
+        register: function(options, next){
+          return next();
+        },
+        execute: function(){
+          return mockedValue;
         }
-      };
-    });
+      }
+    };
+  });
 };
 
 var registerDynamicMocks = function(mocks, logger){
-    return _.map(mocks, function(source, pluginName){
-      var p;
-      try {
-        p = require(path.resolve(source));
-      } catch(er) {
-        logger.log(er.toString().red);
-        return;
-      }
+  return _.map(mocks, function(source, pluginName){
+    var p;
+    try {
+      p = require(path.resolve(source));
+    } catch(er) {
+      logger.log(er.toString().red);
+      return;
+    }
 
-      logger.log('├── '.green + pluginName + ' () => [Function]');
-      return {
-        name: pluginName,
-        register: {
-          register: function(options, next){
-            return next();
-          },
-          execute: p
-        }
-      };
-    });
+    logger.log('├── '.green + pluginName + ' () => [Function]');
+    return {
+      name: pluginName,
+      register: {
+        register: function(options, next){
+          return next();
+        },
+        execute: p
+      }
+    };
+  });
 };
 
 module.exports = function(logger){
