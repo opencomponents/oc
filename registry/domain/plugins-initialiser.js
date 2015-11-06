41dacbf7afc517cfe51a7146abe93647406b945b
Andy Royle <ajroyle@gmail.com>
2015-11-06 14:39:14 +0000

    allow plugins to depend on other plugins

--- registry/domain/plugins-initialiser.js
+++ registry/domain/plugins-initialiser.js
@@ -3,26 +3,106 @@
 var async = require('async');
 var format = require('stringformat');
 var _ = require('underscore');
+var madge = require('madge');
 
 var strings = require('../../resources');
+
+var checkDependencies = function(plugins){
+    var dependencies = {};
+
+    plugins.forEach(function(p){
+      if(!p.register.dependencies){
+        dependencies[p.name] = [];
+        return;
+      }
+
+      dependencies[p.name] = p.register.dependencies;
+    });
+
+    var missing = _.difference(_.flatten(_.values(dependencies)), _.pluck(plugins, 'name'));
+
+    if(missing.length > 0){
+      throw new Error('unknown plugin dependency(s) :' + JSON.stringify(missing));
+    }
+
+    var tree = madge(dependencies, { findNestedDependencies: true });
+    var circular = tree.circular().getArray();
+    if(circular.length){
+        throw new Error('circular dependency detected in plugins: ' + circular);
+    }
+
+    return tree;
+};
+
+var deferredLoads = [];
+var defer = function(plugin, cb){
+  deferredLoads.push(plugin);
+  return cb();
+};
 
 module.exports.init = function(pluginsToRegister, callback){
 
   var c = 0,
       registered = {};
 
-  async.mapSeries(pluginsToRegister, function(plugin, cb){
+  try {
+      checkDependencies(pluginsToRegister);
+  }
+  catch(err){
+      return callback(err);
+  }
 
-    c++;
-    if(!_.isObject(plugin.register) || !_.isFunction(plugin.register.register) || 
-       !_.isFunction(plugin.register.execute) || !_.isString(plugin.name)){
-      return cb(format(strings.errors.registry.PLUGIN_NOT_VALID, c));
+  var dependenciesRegistered = function(dependencies){
+    if(dependencies.length === 0){
+      return true;
     }
 
-    plugin.register.register(plugin.options || {}, plugin.callback || _.noop);
-    registered[plugin.name] = plugin.register.execute;    
+    var present = true;
+    dependencies.forEach(function(d) {
+      if(!registered[d]){
+        present = false;
+      }
+    });
+
+    return present;
+  };
+
+  var loadPlugin = function(plugin, cb){
+    c++;
+    if(!_.isObject(plugin.register) || !_.isFunction(plugin.register.register) ||
+       !_.isFunction(plugin.register.execute) || !_.isString(plugin.name)){
+      return cb(new Error(format(strings.errors.registry.PLUGIN_NOT_VALID, c)));
+    }
+
+    if(registered[plugin.name]){
+      return cb();
+    }
+
+    if(!plugin.register.dependencies){
+        plugin.register.dependencies = [];
+    }
+
+    if(!dependenciesRegistered(plugin.register.dependencies)){
+      return defer(plugin, cb);
+    }
+
+    var dependencies = _.pick(registered, plugin.register.dependencies);
+
+    plugin.register.register(plugin.options || {}, dependencies, plugin.callback || _.noop);
+    registered[plugin.name] = plugin.register.execute;
     cb();
-  }, function(err){
+  };
+
+  var terminator = function(err){
+    if(deferredLoads.length > 0){
+      var deferredPlugins = _.clone(deferredLoads);
+      deferredLoads = [];
+
+      return async.mapSeries(deferredPlugins, loadPlugin, terminator);
+    }
+
     callback(err, registered);
-  });
+  };
+
+  async.mapSeries(pluginsToRegister, loadPlugin, terminator);
 };
