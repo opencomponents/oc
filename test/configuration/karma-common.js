6190fdc2a21b63f9fc6bd5c7f3fb8f36a4ef7f52
Matteo Figus <matteofigus@gmail.com>
2015-11-30 16:18:00 +0000

    House keeping

--- test/configuration/karma-common.js
+++ test/configuration/karma-common.js
@@ -1,19 +1,26 @@
 'use strict';
 
 module.exports = {
-    basePath: '',
-    frameworks: ['jasmine', 'sinon'],
-    files: [
-      'jquery-1.11.2.js',
-      'jQuery.XDomainRequest.js',
-      '../../components/oc-client/src/head.load.js',
-      '../../node_modules/handlebars/dist/handlebars.runtime.js',
-      '../../node_modules/jade/runtime.js',
-      '../front-end/ocTestConfig.js',
-      '../../components/oc-client/src/oc-client.js',
+  basePath: '',
+  frameworks: ['jasmine', 'sinon'],
+  files: [
 
-      '../front-end/*.js'
-    ],
-    port: 9876,
-    singleRun: false
-  };
+    // Dynamically loaded via oc-client, alredy loaded to speed-up tests
+    'jquery-1.11.2.js',
+    'jQuery.XDomainRequest.js',
+    '../../node_modules/handlebars/dist/handlebars.runtime.js',
+    '../../node_modules/jade/runtime.js',
+
+    // The tests settings
+    'test-settings.js',
+
+    // The oc-client bundle
+    '../../components/oc-client/src/head.load.js',
+    '../../components/oc-client/src/oc-client.js',
+
+    // The tests
+    '../front-end/*.js'
+  ],
+  port: 9876,
+  singleRun: false
+};
