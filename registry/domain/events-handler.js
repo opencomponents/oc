c27a029c08fd88a47164b1336cd18d99b9c87f8c
Matteo Figus <matteofigus@gmail.com>
2016-01-13 18:27:36 +0000

    Testing added for new event

--- registry/domain/events-handler.js
+++ registry/domain/events-handler.js
@@ -6,26 +6,26 @@ var strings = require('../../resources');
 
 var subscriptions = {};
 
-module.exports.fire = function(eventName, eventData){
-  if(!!subscriptions[eventName]){
-    _.forEach(subscriptions[eventName], function(callback){
-      callback(eventData);
-    });
+module.exports = {
+  fire: function(eventName, eventData){
+    if(!!subscriptions[eventName]){
+      _.forEach(subscriptions[eventName], function(callback){
+        callback(eventData);
+      });
+    }
+  },
+  on: function(eventName, callback){
+    if(!_.isFunction(callback)){
+      throw(strings.errors.registry.CONFIGURATION_ONREQUEST_MUST_BE_FUNCTION);
+    }
+
+    if(!subscriptions[eventName]){
+      subscriptions[eventName] = [];
+    }
+
+    subscriptions[eventName].push(callback);
+  },
+  reset: function(){
+    subscriptions = {};
   }
-};
-
-module.exports.on = function(eventName, callback){
-  if(!_.isFunction(callback)){
-    throw(strings.errors.registry.CONFIGURATION_ONREQUEST_MUST_BE_FUNCTION);
-  }
-
-  if(!subscriptions[eventName]){
-    subscriptions[eventName] = [];
-  }
-
-  subscriptions[eventName].push(callback);
-};
-
-module.exports.reset = function(){
-  subscriptions = {};
 };
