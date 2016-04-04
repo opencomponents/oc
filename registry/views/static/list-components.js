6326f19401c7685d74606851cb83919927ddb571
Matteo Figus <matteofigus@gmail.com>
2016-04-04 12:01:01 +0100

    Cleanup

--- registry/views/static/list-components.js
+++ registry/views/static/list-components.js
@@ -34,30 +34,4 @@ oc.cmd.push(function(){
 
   $('#filter-components').submit(componentsListChanged).keyup(componentsListChanged);
   $('#filter-components input[type=checkbox').change(componentsListChanged);
-
-  $('#versions').change(function(){
-    window.location = thisComponentHref + $(this).val() + '/~info';
-  });
-
-  $('.refresh-preview').click(function(){
-
-    var splitted = $('#href').val().split('?'),
-        url = splitted[0],
-        lang = $('#lang').val();
-
-    if(url.slice(-1) !== '/'){
-      url += '/';
-    }
-
-    url = url.replace('http\:\/\/', '\/\/').replace('https\:\/\/', '\/\/');
-    url += '~preview/?__ocAcceptLanguage=' + lang + '&';
-
-    if(splitted.length > 1){
-      url += splitted[1];
-    }
-
-    $('.preview').attr('src', url);
-
-    return false;
-  });
 });
