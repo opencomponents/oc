f4866e36d6dfc3a7518f12dec534a26dd569f95c
Matteo Figus <matteofigus@gmail.com>
2016-04-05 10:13:47 +0100

    Merge pull request #222 from opentable/c-ompre-ss
    Compress inlined js and css inside jade views

---
+++ registry/views/static/list-components.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var oc = oc || {};
+oc.cmd = oc.cmd || [];
+
+oc.cmd.push(function(){
+
+  var componentsListChanged = function(){
+    $('.componentRow').removeClass('hide');
+    var s = $('.search').val(),
+        r = new RegExp(s),
+        selectedCheckboxes = $('input[type=checkbox]:checked'),
+        hiddenStates = [],
+        i;
+
+    for(i = 0; i < selectedCheckboxes.length; i++){
+      hiddenStates.push($(selectedCheckboxes[i]).attr('name'));
+    }
+
+    for(i = 0; i < componentsList.length; i++){
+      var matches = !s || !!componentsList[i].name.match(r),
+          selector = $('#component-' + componentsList[i].name),
+          isHidden = false;
+
+      for(var j = 0; j < hiddenStates.length; j++){
+        if(componentsList[i].state.toLowerCase() === hiddenStates[j]){
+          isHidden = true;
+        }
+      }
+
+      var show = matches && !isHidden;
+      selector[show ? 'removeClass' : 'addClass']('hide');
+    }
+
+    return false;
+  };
+
+  $('#filter-components').submit(componentsListChanged).keyup(componentsListChanged);
+  $('#filter-components input[type=checkbox').change(componentsListChanged);
+});
