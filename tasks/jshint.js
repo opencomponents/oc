3520d6f7555c794d206fd24e21dcb1f763d2ab1f
Andy Royle <ajroyle@gmail.com>
2016-05-05 16:38:54 +0100

    add ignores for examples directory

--- tasks/jshint.js
+++ tasks/jshint.js
@@ -4,7 +4,9 @@ module.exports = {
   options: {
     jshintrc: 'tasks/support/.jshintrc',
     ignores: [
-      'node_modules', 
+      'node_modules',
+      'examples/node_modules', 
+      'examples/**/_package/',
       'client/src/oc-client.min.js', 
       'src/components/oc-client/src/head.load.js', 
       'src/components/oc-client/src/oc-client.min.js', 
