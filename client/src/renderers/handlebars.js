83bc16bbef0c61cbd0b5ce88f2a327d85cd512b7
Matteo Figus <matteofigus@gmail.com>
2016-06-21 01:39:55 +0100

    Added multiple rendering engine for server-side handlebars3+4

--- client/src/renderers/handlebars.js
+++ client/src/renderers/handlebars.js
@@ -1,11 +1,14 @@
 'use strict';
 
 var handlebars = require('handlebars');
+var handlebars3 = require('handlebars3');
 
 module.exports = function(){
 
   this.render = function(template, model, callback){
-    var linked = handlebars.template(template),
+    var needs3 = template.compiler[0] < 7,
+        compiler = needs3 ? handlebars3 : handlebars,
+        linked = compiler.template(template),
         html = linked(model);
 
     callback(null, html);
--- test/unit/registry-domain-repository.js
+++ test/unit/registry-domain-repository.js
@@ -252,7 +252,7 @@ describe('registry : domain : repository', function(){
       });
 
       it('should list the components', function(){
-        expect(response.result).to.eql(['hello-world', 'language', 'no-containers', 'welcome', 'oc-client']);
+        expect(response.result).to.eql(['hello-world', 'hello-world-handlebars3', 'language', 'no-containers', 'welcome', 'oc-client']);
       });
     });
 
