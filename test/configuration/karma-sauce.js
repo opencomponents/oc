e9b301945b87dbea16260716309b7fb0a8bf511b
Matteo Figus <matteofigus@gmail.com>
2016-09-13 01:22:49 +0100

    Wow almost there. Cleanup

--- test/configuration/karma-sauce.js
+++ test/configuration/karma-sauce.js
@@ -22,8 +22,7 @@ module.exports = function(config) {
 
   if (process.env.TRAVIS)
   {
-    config.logLevel = config.LOG_DEBUG;
-    //config.transports = ['xhr-polling'];
+    //config.logLevel = config.LOG_DEBUG;
     config.sauceLabs.build = 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')';
     config.sauceLabs.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
   }
