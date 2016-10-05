fa452ea7b31e0d3dcbcbef289c1b466f81532239
Matteo Figus <matteofigus@gmail.com>
2016-10-05 11:39:58 +0100

    Removing jade dependency as what we need is just the client-side runtime

--- client/src/executor.js
+++ client/src/executor.js
@@ -4,7 +4,7 @@ var vm = require('vm');
 
 module.exports = {
   template: function(code, template){
-    var context = template.type === 'jade' ? { jade: require('jade/runtime.js')} : {};
+    var context = template.type === 'jade' ? { jade: require('./renderers/support/jade-runtime.js')} : {};
     vm.runInNewContext(code, context);
     return context.oc.components[template.key];
   }
