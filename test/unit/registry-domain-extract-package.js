265246c686c328976545005434e7d44f01bb3764
Matteo Figus <matteofigus@gmail.com>
2016-11-04 17:56:50 +0000

    Added some more testing and reverted the order

--- test/unit/registry-domain-extract-package.js
+++ test/unit/registry-domain-extract-package.js
@@ -12,15 +12,15 @@ describe('registry : domain : extract-package', function(){
   var extractPackage = injectr('../../src/registry/domain/extract-package.js', {
     targz: { decompress: decompressStub },
     path: { resolve: pathResolveStub },
-    './get-package-json-from-temp-dir': sinon.stub().yields(null, {})
-  });
+    './get-package-json-from-temp-dir': sinon.stub().yields(null, { package: 'hello' })
+  }, { console: console});
 
   describe('when extracting package', function(){
 
     var error, response;
 
     beforeEach(function(done){
-
+      pathResolveStub.reset();
       pathResolveStub.onCall(0).returns('/some-path/registry/temp/1478279453422.tar.gz');
       pathResolveStub.onCall(1).returns('/some-path/registry/temp/1478279453422/');
       pathResolveStub.onCall(2).returns('/some-path/registry/temp/1478279453422/_package/');
@@ -41,8 +41,15 @@ describe('registry : domain : extract-package', function(){
 
     it('should decompress tar.gz file', function(){
       expect(decompressStub.args[0][0]).to.eql({
-        dest: '/some-path/registry/temp/1478279453422/',
-        src: '/some-path/registry/temp/1478279453422.tar.gz'
+        src: '/some-path/registry/temp/1478279453422.tar.gz',
+        dest: '/some-path/registry/temp/1478279453422/'
+      });
+    });
+
+    it('should respond', function(){
+      expect(response).to.eql({
+        outputFolder: '/some-path/registry/temp/1478279453422/_package/',
+        packageJson: { package: 'hello' }
       });
     });
   });
