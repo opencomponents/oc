opencomponents/oc

commit cc2fdfffaa168e0c30dec4b05c7bf1cfb92f03ad

Add optional cache headers for components (#325) (#326)
* Added a function setHeader(name, value) to the context object that we pass to the component data function.

* Subsequently these headers (if added) will be set in the response data in the returned component JSON as well as response headers (component.js)

* Added a mock component (response-headers-component) and a test to cover the new functionality

src/registry/routes/helpers/get-component.js
@@ -26,7 +26,8 @@ module.exports = function(conf, repository){
       cache = new Cache({
         verbose: !!conf.verbosity,
         refreshInterval: conf.refreshInterval
-      });
+      }),
+      responseHeaders;
 
   var renderer = function(options, cb){
 
@@ -188,6 +189,10 @@ module.exports = function(conf, repository){
                 });
               }
 
+              if (responseHeaders) {
+                response.headers = responseHeaders;
+              }
+
               callback({
                 status: 200, 
                 response: _.extend(response, { html: html })
@@ -225,7 +230,11 @@ module.exports = function(conf, repository){
               renderComponent: nestedRenderer.renderComponent,
               renderComponents: nestedRenderer.renderComponents,
               requestHeaders: options.headers,
-              staticPath: repository.getStaticFilePath(component.name, component.version, '').replace('https:', '')
+              staticPath: repository.getStaticFilePath(component.name, component.version, '').replace('https:', ''),
+              setHeader: function(header, value) {
+                responseHeaders = responseHeaders || {};
+                responseHeaders[header] = value;
+              }
             };
 
         var setCallbackTimeout = function(){

test/fixtures/mocked-components/index.js
@@ -7,5 +7,6 @@ module.exports = {
   'npm-component': require('./npm'),
   'plugin-component': require('./plugin'),
   'timeout-component': require('./timeout'),
-  'undefined-component': require('./undefined')
+  'undefined-component': require('./undefined'),
+  'response-headers-component': require('./response-headers')
 };

test/unit/registry-routes-component.js
@@ -417,4 +417,53 @@ describe('registry : routes : component', function(){
       });
     });
   });
+
+  describe('when getting a component with server.js that sets custom headers', function() {
+
+    var code, response, headers;
+
+    before(function(done) {
+      initialise(mockedComponents['response-headers-component']);
+      componentRoute = new ComponentRoute({}, mockedRepository);
+
+      var resJson = function(calledCode, calledResponse) {
+        code = calledCode;
+        response = calledResponse;
+        done();
+      };
+
+      var resSet = function(calledHeaders) {
+        headers = calledHeaders;
+      };
+
+      componentRoute({
+        headers: {},
+        params: { componentName: 'response-headers-component', componentVersion: '1.X.X' }
+      }, {
+        conf: {
+          baseUrl: 'http://component.com/',
+          executionTimeout: 0.1
+        },
+        json: resJson,
+        set: resSet
+      });
+    });
+
+    it('should return 200 status code', function() {
+      expect(code).to.be.equal(200);
+    });
+
+    it('should set response headers', function() {
+      expect(response.headers).to.not.be.null;      
+      expect(response.headers['test-header']).to.equal('test-value');
+      expect(headers).to.not.be.null;      
+      expect(headers['test-header']).to.equal('test-value');
+    });
+
+    it('should return component\'s name and request version', function() {
+      expect(response.name).to.equal('response-headers-component');
+      expect(response.requestVersion).to.equal('1.X.X');
+    });
+  });
+
 });

src/registry/routes/component.js
@@ -19,6 +19,10 @@ module.exports = function(conf, repository){
         res.errorDetails = result.response.error;
       }
 
+      if (result.response.headers) {
+        res.set(result.response.headers);
+      }
+
       return res.json(result.status, result.response);
     });
   };

test/fixtures/mocked-components/response-headers.js
@@ -1,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  package: {
+    name: 'response-headers-component',
+    version: '1.0.0',
+    oc: {
+      container: false,
+      renderInfo: false,
+      files: {
+        template: {
+          type: 'jade',
+          hashKey: '8c1fbd954f2b0d8cd5cf11c885fed4805225749f',
+          src: 'template.js'
+        },
+        dataProvider: {
+          type: 'node.js',
+          hashKey: '123456',
+          src: 'server.js'
+        }
+      }
+    }
+  },
+  data: '"use strict";module.exports.data = function(ctx, cb){ctx.setHeader("test-header","test-value"); cb(null, {done:true});};',
+  view: 'var oc=oc||{};oc.components=oc.components||{},oc.components["8c1fbd954f2b0d8cd5cf11c885fed4805225749f"]' +
+        '=function(){var o=[];return o.push("<div>hello</div>"),o.join("")};'
+};

commit 9064e3526ad704b0656b34a1097e9d086255e7c9

Fix for Custom eaders of one component can appear to another.
The problem was introduced with #326 -
In get-component.js the declaration of responseHeaders variable happens in a
more global scope than it should be. For this reason every invocation of
renderer function sees the changes in the headers made by previously requested
components.
The fix is to just move the declaration one scope inner.

- Added a unit test to validate the fix - it tries to load two components
subsequently. The first one does provide custom headers while the second
one - don't. Then we check and expect to see there are no custom headers
in the result for the second one.

src/registry/routes/helpers/get-component.js
@@ -26,13 +26,13 @@ module.exports = function(conf, repository){
       cache = new Cache({
         verbose: !!conf.verbosity,
         refreshInterval: conf.refreshInterval
-      }),
-      responseHeaders;
+      });
 
   var renderer = function(options, cb){
 
     var nestedRenderer = new NestedRenderer(renderer, options.conf),
-        retrievingInfo = new GetComponentRetrievingInfo(options);
+        retrievingInfo = new GetComponentRetrievingInfo(options),
+        responseHeaders;
 
     var getLanguage = function(){
       var paramOverride = !!options.parameters && options.parameters['__ocAcceptLanguage'];

test/fixtures/mocked-components/another-response-headers.js
@@ -1,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  package: {
+    name: 'another-response-headers-component',
+    version: '1.0.0',
+    oc: {
+      container: false,
+      renderInfo: false,
+      files: {
+        template: {
+          type: 'jade',
+          hashKey: '8c1fbd954f2b0d8cd5cf11c885fed4805225749f',
+          src: 'template.js'
+        },
+        dataProvider: {
+          type: 'node.js',
+          hashKey: '123456',
+          src: 'server.js'
+        }
+      }
+    }
+  },
+  data: '"use strict";module.exports.data = function(ctx, cb){ctx.setHeader("another-test-header","another-test-value"); cb(null, {done:true});};',
+  view: 'var oc=oc||{};oc.components=oc.components||{},oc.components["8c1fbd954f2b0d8cd5cf11c885fed4805225749f"]' +
+        '=function(){var o=[];return o.push("<div>hello</div>"),o.join("")};'
+};

test/fixtures/mocked-components/index.js
@@ -8,5 +8,7 @@ module.exports = {
   'plugin-component': require('./plugin'),
   'timeout-component': require('./timeout'),
   'undefined-component': require('./undefined'),
-  'response-headers-component': require('./response-headers')
+  'simple-component': require('./simple'),
+  'response-headers-component': require('./response-headers'),
+  'another-response-headers-component': require('./another-response-headers')
 };

test/fixtures/mocked-components/simple.js
@@ -1,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  package: {
+    name: 'simple-component',
+    version: '1.0.0',
+    oc: {
+      container: false,
+      renderInfo: false,
+      files: {
+        template: {
+          type: 'jade',
+          hashKey: '8c1fbd954f2b0d8cd5cf11c885fed4805225749f',
+          src: 'template.js'
+        },
+        dataProvider: {
+          type: 'node.js',
+          hashKey: '123457',
+          src: 'server.js'
+        }
+      }
+    }
+  },
+  data: '"use strict";module.exports.data = function(ctx, cb){cb(null, {done:true});};',
+  view: 'var oc=oc||{};oc.components=oc.components||{},oc.components["8c1fbd954f2b0d8cd5cf11c885fed4805225749f"]' +
+        '=function(){var o=[];return o.push("<div>hello</div>"),o.join("")};'
+};

test/unit/registry-routes-component.js
@@ -419,7 +419,6 @@ describe('registry : routes : component', function(){
   });
 
   describe('when getting a component with server.js that sets custom headers', function() {
-
     var code, response, headers;
 
     before(function(done) {
@@ -463,6 +462,106 @@ describe('registry : routes : component', function(){
     it('should return component\'s name and request version', function() {
       expect(response.name).to.equal('response-headers-component');
       expect(response.requestVersion).to.equal('1.X.X');
+    });
+  });
+
+  describe('when getting a simple component with server.js after headers component no custom headers should be set', function() {
+    var code={}, 
+      response={}, 
+      headers={};
+
+    before(function(done) {
+      var headersComponent = mockedComponents['another-response-headers-component'];
+      var simpleComponent = mockedComponents['simple-component'];
+
+      mockedRepository = {
+        getCompiledView: sinon.stub(),
+        getComponent: sinon.stub(),
+        getDataProvider: sinon.stub(),
+        getStaticFilePath: sinon.stub().returns('//my-cdn.com/files/')
+      };
+
+      //Custom repository initialization to give us two components when invoked twice...
+      //...the firts one with custom headers and the second - without.
+      mockedRepository.getCompiledView.onCall(0).yields(null, headersComponent.view);
+      mockedRepository.getCompiledView.onCall(1).yields(null, simpleComponent.view);
+
+      mockedRepository.getComponent.onCall(0).yields(null, headersComponent.package);
+      mockedRepository.getComponent.onCall(1).yields(null, simpleComponent.package);
+
+      mockedRepository.getDataProvider.onCall(0).yields(null, headersComponent.data);
+      mockedRepository.getDataProvider.onCall(1).yields(null, simpleComponent.data);
+
+      componentRoute = new ComponentRoute({}, mockedRepository);
+      resJsonStub = sinon.stub();
+
+      var resJson = function(index, callback) { 
+        return function(calledCode, calledResponse) {
+          code[index] = calledCode;
+          response[index] = calledResponse;
+          callback && callback();
+        };
+      };
+
+      var resSet = function(index) { 
+        return function(calledHeaders) {
+          headers[index] = calledHeaders;
+        };
+      };
+
+      var requestComponent = function(componentName, resultIndex) {
+        return new Promise(function(resolve, reject) {
+          componentRoute({
+            headers: {},
+            params: { componentName: componentName, componentVersion: '1.X.X' }
+          }, {
+            conf: {
+              baseUrl: 'http://component.com/',
+              executionTimeout: 0.1
+            },
+            json: resJson(resultIndex, function() { resolve(); }),
+            set: resSet(resultIndex)
+          });
+        });
+      };
+
+      requestComponent('another-response-headers-component', 0)
+      .then(requestComponent('simple-component', 1))
+      .then(function() { done(); });
+    });
+
+    //The first part of the test - checking that another-response-headers-component 
+    //should return a response with custom headers
+    it('should return 200 status code for the first component', function() {
+      expect(code[0]).to.be.equal(200);
+    });
+
+    it('should return "response-headers-component" name for the first component\'s name and request version', function() {
+      expect(response[0].name).to.equal('another-response-headers-component');
+      expect(response[0].requestVersion).to.equal('1.X.X');
+    });
+
+    it('should set response headers for the first component', function() {
+      expect(response[0].headers).to.not.be.null;      
+      expect(response[0].headers['another-test-header']).to.equal('another-test-value');
+      expect(headers[0]).to.not.be.null;      
+      expect(headers[0]['another-test-header']).to.equal('another-test-value');
+    });
+
+    //The second part of the test - validating that simple-component should not return 
+    //the custom headers previously set by another-headers-component.
+    it('should return 200 status code for the first component', function() {
+      expect(code[1]).to.be.equal(200);
+    });
+
+    it('should return "simple-component" name for the first component\'s name and request version', function() {
+      expect(response[1].name).to.equal('simple-component');
+      expect(response[1].requestVersion).to.equal('1.X.X');
+    });
+
+    it('should not set custom response', function() {
+      expect(response[1].headers).to.be.undefined;      
+      expect(headers[1]).to.be.undefined;      
     });
   });
 