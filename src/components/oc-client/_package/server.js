e518d91d7ce8d80507b1612898274d7ddfe313c1
Matteo Figus <matteofigus@gmail.com>
2017-01-20 11:38:34 +0000

    0.34.0

--- src/components/oc-client/_package/server.js
+++ src/components/oc-client/_package/server.js
@@ -1,1 +1,1 @@
-"use strict";module.exports.data=function(t,a){a(null,{staticPath:t.staticPath})};
+module.exports=function(t){function r(o){if(e[o])return e[o].exports;var n=e[o]={exports:{},id:o,loaded:!1};return t[o].call(n.exports,n,n.exports,r),n.loaded=!0,n.exports}var e={};return r.m=t,r.c=e,r.p="",r(0)}([function(t,r){"use strict";t.exports.data=function(t,r){r(null,{staticPath:t.staticPath})}}]);
