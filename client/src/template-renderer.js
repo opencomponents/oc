67d0d3aa61718fee6c68059cece8867155afb511
Nick Balestra <nbalestra@opentable.com>
2017-01-25 14:06:28 +0000

    rely on handlebar template render logic and validation from oc-template-handlebars

--- client/src/template-renderer.js
+++ client/src/template-renderer.js
@@ -1,28 +1,27 @@
 'use strict';
 
-var Handlebars = require('./renderers/handlebars');
+var handlebars = require('oc-template-handlebars');
 var htmlRenderer = require('./html-renderer');
 var Jade = require('./renderers/jade');
 var validator = require('./validator');
 
 module.exports = function(){
-  
   var renderers = {
-    handlebars: new Handlebars(),
+    handlebars,
     jade: new Jade()
   };
 
-  return function(template, data, options, callback){
-
-    var validationResult = validator.validateComponent(template, options);
-
-    if(!validationResult.isValid){
-      return callback(validationResult.error);
-    }
-
-    renderers[options.templateType].render(template, data, function(err, html){
-      options.html = html;
-      return callback(err, htmlRenderer.renderedComponent(options));
-    });
+  return function(template, model, options, callback){
+    renderers[options.templateType].render(
+      {
+        template,
+        model,
+        templateType: options.templateType
+      },
+      function(err, html){
+        options.html = html;
+        return callback(err, htmlRenderer.renderedComponent(options));
+      }
+    );
   };
 };
