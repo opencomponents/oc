1578edb25ecdc120fc7e1eee2549639add5355a9
Nick Balestra <nbalestra@opentable.com>
2017-02-28 14:57:18 +0000

    new oc-template compile api and added support for template-react

--- client/src/template-renderer.js
+++ client/src/template-renderer.js
@@ -3,12 +3,14 @@
 var handlebars = require('oc-template-handlebars');
 var htmlRenderer = require('./html-renderer');
 var jade = require('oc-template-jade');
+var react = require('oc-template-react');
 var validator = require('./validator');
 
 module.exports = function(){
   var templateEngines = {
     handlebars,
-    jade
+    jade,
+    react
   };
 
   return function(template, model, options, callback){
--- src/cli/domain/package-template.js
+++ src/cli/domain/package-template.js
@@ -4,6 +4,7 @@ var format = require('stringformat');
 var fs = require('fs-extra');
 var handlebars = require('oc-template-handlebars');
 var jade = require('oc-template-jade');
+var react = require('oc-template-react');
 var path = require('path');
 var uglifyJs = require('uglify-js');
 
@@ -13,28 +14,30 @@ var validator = require('../../registry/domain/validators');
 
 var templateEngines = {
   handlebars,
-  jade
+  jade,
+  react
 };
 
 var javaScriptizeTemplate = function(functionName, data){
   return format('var {0}={0}||{};{0}.components={0}.components||{};{0}.components[\'{1}\']={2}', 'oc', functionName, data.toString());
 };
 
-var compileView = function(viewPath, type) {
+var compileView = function(viewPath, type, cb) {
   var template = fs.readFileSync(viewPath).toString();
 
   if(!templateEngines[type]){
     throw strings.errors.cli.TEMPLATE_TYPE_NOT_VALID;
   }
 
-  var compiledView = templateEngines[type].compile(template, { viewPath }),
-      hashView = hashBuilder.fromString(compiledView.toString()),
-      javaScriptizedView = javaScriptizeTemplate(hashView, compiledView);
+  templateEngines[type].compile({ template, viewPath}, (compiledView) => {
+    var hashView = hashBuilder.fromString(compiledView.toString()),
+        javaScriptizedView = javaScriptizeTemplate(hashView, compiledView);
 
-  return {
-    hash: hashView,
-    view: uglifyJs.minify(javaScriptizedView, {fromString: true}).code
-  };
+    cb({
+      hash: hashView,
+      view: uglifyJs.minify(javaScriptizedView, {fromString: true}).code
+    });
+  });
 };
 
 module.exports = function(params, callback){
@@ -48,17 +51,16 @@ module.exports = function(params, callback){
   }
 
   try {
-    compiled = compileView(viewPath, params.ocOptions.files.template.type);
+    compileView(viewPath, params.ocOptions.files.template.type, (compiled) => {
+      fs.writeFile(path.join(params.publishPath, 'template.js'), compiled.view, function(err, res){
+        callback(err, {
+          type: params.ocOptions.files.template.type,
+          hashKey: compiled.hash,
+          src: 'template.js'
+        });
+      });
+    });
   } catch(e){
     return callback(format('{0} compilation failed - {1}', viewSrc, e));
   }
-
-  fs.writeFile(path.join(params.publishPath, 'template.js'), compiled.view, function(err, res){
-    callback(err, {
-      type: params.ocOptions.files.template.type,
-      hashKey: compiled.hash,
-      src: 'template.js'
-    });
-  });
-
 };
