f7ecbafdcb0928a3cacffa469873825450322288
Nick Balestra <nbalestra@opentable.com>
2017-03-31 14:46:57 +0100

    try-catch requiring strategy

--- client/src/utils/require-template.js
+++ client/src/utils/require-template.js
@@ -8,14 +8,21 @@ var templateNotSupported = 'Error loading component: template "{0}" not supporte
 
 module.exports = function(template) {
   var localTemplate = path.join(__dirname, '../../', 'node_modules', template);
-  if (fs.existsSync(localTemplate)) {
-    return require(localTemplate);
-  }
-
   var relativeTemplate = path.resolve('.', 'node_modules', template);
-  if (fs.existsSync(relativeTemplate)) {
-    return require(relativeTemplate);
-  }
 
-  throw format(templateNotSupported, template);
+  try {
+    if (!!require.cache[localTemplate]) {
+      delete require.cache[localTemplate];
+    }
+    return require(localTemplate);
+  } catch(err) {
+    try {
+      if (!!require.cache[relativeTemplate]) {
+        delete require.cache[relativeTemplate];
+      }
+      return require(relativeTemplate);
+    } catch (err) {
+      throw format(templateNotSupported, template);
+    }
+  }
 };
--- src/utils/require-template.js
+++ src/utils/require-template.js
@@ -6,16 +6,23 @@ var fs = require('fs');
 
 var templateNotSupported = 'Error requiring the template "{0}": oc-template not found';
 
-module.exports = function(template) {  
+module.exports = function(template) {
   var localTemplate = path.join(__dirname, '../../', 'node_modules', template);
-  if (fs.existsSync(localTemplate)) {
-    return require(localTemplate);
-  }
-
   var relativeTemplate = path.resolve('.', 'node_modules', template);
-  if (fs.existsSync(relativeTemplate)) {
-    return require(relativeTemplate);
-  }
 
-  throw format(templateNotSupported, template);
+  try {
+    if (!!require.cache[localTemplate]) {
+      delete require.cache[localTemplate];
+    }
+    return require(localTemplate);
+  } catch(err) {
+    try {
+      if (!!require.cache[relativeTemplate]) {
+        delete require.cache[relativeTemplate];
+      }
+      return require(relativeTemplate);
+    } catch (err) {
+      throw format(templateNotSupported, template);
+    }
+  }
 };
