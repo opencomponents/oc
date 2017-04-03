b6efbb1b2c6eb2949b622fa65bb678e90a269a32
Nick Balestra <nbalestra@opentable.com>
2017-04-03 20:33:05 +0100

    cli init now allow for template to support initialization
    shrinkwrap
    
    ora replacement
    
    blueprint and error handling
    
    shwrinkrap
    
    test wip
    
    dependency injection for logger
    
    fixed init.js tests
    
    init-template cleanup

--- src/cli/domain/local.js
+++ src/cli/domain/local.js
@@ -5,11 +5,12 @@ const path = require('path');
 const targz = require('targz');
 const _ = require('lodash');
 
-const getComponentsByDir = require('./get-components-by-dir');
-const getLocalNpmModules = require('./get-local-npm-modules');
-const packageComponents = require('./package-components');
-const mock = require('./mock');
-const validator = require('../../registry/domain/validators');
+var getComponentsByDir = require('./get-components-by-dir');
+var getLocalNpmModules = require('./get-local-npm-modules');
+var packageComponents = require('./package-components');
+var mock = require('./mock');
+var validator = require('../../registry/domain/validators');
+var initTemplate = require('./init-template');
 
 module.exports = function() {
   return _.extend(this, {
@@ -34,34 +35,39 @@ module.exports = function() {
     },
     getComponentsByDir: getComponentsByDir(),
     getLocalNpmModules: getLocalNpmModules(),
-    init: function(componentName, templateType, callback) {
-      if (!validator.validateComponentName(componentName)) {
+    init: function(componentName, templateType, options, callback){
+
+      if(!validator.validateComponentName(componentName)){
         return callback('name not valid');
       }
 
-      if (!validator.validateTemplateType(templateType)) {
-        return callback('template type not valid');
+      // LEGACY TEMPLATES
+      if(validator.validateTemplateType(templateType)){
+        try {
+          const pathDir = '../../components/base-component-' + templateType,
+              baseComponentDir = path.resolve(__dirname, pathDir),
+              npmIgnorePath = path.resolve(__dirname, pathDir + '/.npmignore');
+
+          fs.ensureDirSync(componentName);
+          fs.copySync(baseComponentDir, componentName);
+          fs.copySync(npmIgnorePath, componentName + '/.gitignore');
+
+          const componentPath = path.resolve(componentName, 'package.json'),
+            component = _.extend(fs.readJsonSync(componentPath), {
+              name: componentName
+            });
+
+          fs.outputJsonSync(componentPath, component);
+
+          return callback(null, { ok: true });
+        } catch(e){
+          return callback(e);
+        }
       }
-
       try {
-        const pathDir = '../../components/base-component-' + templateType,
-          baseComponentDir = path.resolve(__dirname, pathDir),
-          npmIgnorePath = path.resolve(__dirname, pathDir + '/.npmignore');
-
-        fs.ensureDirSync(componentName);
-        fs.copySync(baseComponentDir, componentName);
-        fs.copySync(npmIgnorePath, componentName + '/.gitignore');
-
-        const componentPath = path.resolve(componentName, 'package.json'),
-          component = _.extend(fs.readJsonSync(componentPath), {
-            name: componentName
-          });
-
-        fs.outputJsonSync(componentPath, component, { spaces: 2 });
-
-        return callback(null, { ok: true });
+        initTemplate(componentName, templateType, options, callback);
       } catch (e) {
-        return callback(e);
+        return callback('template type not valid');
       }
     },
     mock: mock(),
