595a2b35896ab8a6b20ea919b97f46fb998a48b1
Matteo Figus <matteofigus@gmail.com>
2017-04-13 18:17:54 +0100

    Dedicated task to speed up testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function(grunt){
   grunt.registerTask('sauce', ['karma:sauce-linux', 'karma:sauce-osx', 'karma:sauce-windows']);
   grunt.registerTask('test-local', ['eslint', 'mochaTest:unit', 'mochaTest:acceptance', 'karma:local']);
   grunt.registerTask('test-local-silent', ['eslint', 'mochaTest:silent', 'karma:local']);
-  grunt.registerTask('test', ['eslint', 'mochaTest:unit', 'mochaTest:integration', 'mochaTest:acceptance']);
+  grunt.registerTask('test', ['eslint', 'mochaTest:all']);
   grunt.registerTask('git-stage', [
     'gitadd:versionFiles',
     'gitcommit:version',
