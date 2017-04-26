a59c88c7dbda2f8946a405034b4c30aa021c1a2d
Matteo Figus <matteofigus@gmail.com>
2017-04-26 11:03:13 +0200

    Consistency

--- test/unit/registry-domain-events-handler.js
+++ test/unit/registry-domain-events-handler.js
@@ -3,7 +3,7 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
 
-describe('registry : events-handler', () => {
+describe('registry : domain : events-handler', () => {
 
   const eventsHandler = require('../../src/registry/domain/events-handler');
 
