960377c68a52b1a0b82e8cbca738cadfb3696a4b
Nick Balestra <nbalestra@opentable.com>
2017-05-09 16:22:06 +0100

    using devDependencies for template

--- src/cli/domain/init-template/installTemplate.js
+++ src/cli/domain/init-template/installTemplate.js
@@ -3,16 +3,19 @@
 const Spinner = require('cli-spinner').Spinner;
 const spawn = require('cross-spawn');
 const colors = require('colors/safe');
+const path = require('path');
 
-module.exports = function installTemplate(config, callback) {
+module.exports = function installTemplate(config, blueprint) {
   const templateType = config.templateType;
   const cli = config.cli;
   const componentPath = config.componentPath;
   const local = config.local;
   const packageName = config.packageName;
   const logger = config.logger;
+  const callback = config.callback;
+  const installPath = path.resolve(componentPath, '../');
 
-  const installing = new Spinner(`Installing ${packageName} from ${local ? 'local' : 'npm'}...`);
+  const installing = new Spinner(`🚚  Installing ${packageName} from ${local ? 'local' : 'npm'}...`);
   installing.start();
 
   const args = {
@@ -20,11 +23,11 @@ module.exports = function installTemplate(config, callback) {
       'install',
       '--save',
       '--save-exact',
-      templateType
+      local ? path.resolve(process.cwd(), templateType) : templateType,
     ]
   };
-
-  const installProc = spawn(cli, args[cli], {silent: true, cwd: componentPath});
+ 
+  const installProc = spawn(cli, args[cli], {silent: true, cwd: installPath});
 
   installProc.on('error', () => callback('template type not valid'));
   installProc.on('close', code => {
@@ -35,6 +38,23 @@ module.exports = function installTemplate(config, callback) {
     logger.log(
       `${colors.green('✔')} Installed ${packageName} from ${local ? templateType : 'npm'}`
     );
-    return callback(config);
+
+    // install devDependencies
+    const installedTemplatePath = path.resolve(installPath, 'node_modules', templateType)
+    logger.log(
+      `🚚  Installing required devDependencies`
+    );
+
+    const installDevDeps = spawn(cli, ['install'], {stdio: 'inherit', silent: true, cwd: installedTemplatePath});
+
+    installDevDeps.on('close', code => {
+      if (code !== 0) {
+        return callback('template type not valid');
+      }
+      logger.log(
+        `${colors.green('✔')} Installed ${packageName}'s devDependencies`
+      );
+      return blueprint(config);
+    })
   });
 };
