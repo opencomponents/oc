f0a56d16c16732337c18cba4d429b93d6a43d7aa
mattia richetto <mattia.richetto@gmail.com>
2017-05-23 22:52:34 -0700

    get template by name

--- src/utils/require-template.js
+++ src/utils/require-template.js
@@ -6,7 +6,7 @@ const path = require('path');
 const templateNotFound = 'Error requiring oc-template: "{0}" not found';
 const templateNotValid = 'Error requiring oc-template: "{0}" is not a valid oc-template';
 
-function isValidTemplate(template){
+function isValidTemplate(template) {
   if (typeof template !== 'object') {
     return false;
   }
@@ -19,30 +19,34 @@ function isValidTemplate(template){
   ].every((method) => template[method]);
 }
 
+const getOcTemplate = (path) => {
+  if (require.cache && !!require.cache[path]) {
+    delete require.cache[path];
+  }
+  return require(path);
+};
 
-module.exports = function(template) {
+module.exports = function (template) {
   let ocTemplate;
   const localTemplate = path.join(__dirname, '../../', 'node_modules', template);
   const relativeTemplate = path.resolve('.', 'node_modules', template);
 
   try {
-    if (require.cache && !!require.cache[localTemplate]) {
-      delete require.cache[localTemplate];
-    }
-    ocTemplate = require(localTemplate);
-  } catch(err) {
+    ocTemplate = getOcTemplate(template);
+  } catch (err) {
     try {
-      if (require.cache && !!require.cache[relativeTemplate]) {
-        delete require.cache[relativeTemplate];
-      }
-      ocTemplate = require(relativeTemplate);
+      ocTemplate = getOcTemplate(localTemplate);
     } catch (err) {
-      throw format(templateNotFound, template);
+      try {
+        ocTemplate = getOcTemplate(relativeTemplate);
+      } catch (err) {
+        throw new Error(format(templateNotFound, template));
+      }
     }
   }
 
   if (!isValidTemplate(ocTemplate)) {
-    throw format(templateNotValid, template);
+    throw new Error(format(templateNotValid, template));
   }
 
   return ocTemplate;
