eb1526e7bedf2cfb49b3a72d3cf87208dbe23ec9
Nick Balestra <nbalestra@opentable.com>
2017-06-05 17:32:38 +0100

    handle template && template-compiler API

--- src/utils/require-template.js
+++ src/utils/require-template.js
@@ -7,20 +7,41 @@ const templateNotFound = 'Error requiring oc-template: "{0}" not found';
 const templateNotValid =
   'Error requiring oc-template: "{0}" is not a valid oc-template';
 
+<<<<<<< HEAD
 function isValidTemplate(template) {
+=======
+function isValidTemplate(template, options){
+>>>>>>> handle template && template-compiler API
   if (typeof template !== 'object') {
     return false;
   }
 
+<<<<<<< HEAD
   return ['getInfo', 'getCompiledTemplate', 'compile', 'render'].every(
     method => template[method]
   );
 }
 
 module.exports = function(template) {
+=======
+  if (options.compiler === true) {
+    return typeof template.compile === 'function';
+  }
+
+  return [
+    'getInfo',
+    'getCompiledTemplate',
+    'render'
+  ].every((method) => typeof template[method] === 'function');
+}
+
+module.exports = function(template, options) {
+  requireOptions = options || {};
+>>>>>>> handle template && template-compiler API
   let ocTemplate;
   if (template === 'jade') { template = 'oc-template-jade'; }
   if (template === 'handlebars') { template = 'oc-template-handlebars'; }
+  if (requireOptions.compiler === true) { template = template + '-compiler'; }
 
   const localTemplate = path.join(__dirname, '../../', 'node_modules', template);
   const relativeTemplate = path.resolve('.', 'node_modules', template);
@@ -41,7 +62,7 @@ module.exports = function(template) {
     }
   }
 
-  if (!isValidTemplate(ocTemplate)) {
+  if (!isValidTemplate(ocTemplate, requireOptions)) {
     throw format(templateNotValid, template);
   }
 
