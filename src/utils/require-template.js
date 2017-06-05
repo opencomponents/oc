cc107770ad8d25dba52357c7c70cb50049c45c20
Nick Balestra <nbalestra@opentable.com>
2017-06-05 17:32:38 +0100

    handle template && template-compiler API

--- src/utils/require-template.js
+++ src/utils/require-template.js
@@ -7,15 +7,20 @@ const templateNotFound = 'Error requiring oc-template: "{0}" not found';
 const templateNotValid =
   'Error requiring oc-template: "{0}" is not a valid oc-template';
 
-function isValidTemplate(template) {
+function isValidTemplate(template, options) {
   if (typeof template !== 'object') {
     return false;
   }
 
-  return ['getInfo', 'getCompiledTemplate', 'compile', 'render'].every(
-    method => template[method]
+  if (options.compiler === true) {
+    return typeof template.compile === 'function';
+  }
+
+  return ['getInfo', 'getCompiledTemplate', 'render'].every(
+    method => typeof template[method] === 'function'
   );
 }
+
 
 const getOcTemplate = (path) => {
   if (require.cache && !!require.cache[path]) {
@@ -24,12 +29,25 @@ const getOcTemplate = (path) => {
   return require(path);
 };
 
-module.exports = function (template) {
+module.exports = function(template, options) {
+  const requireOptions = options || {};
   let ocTemplate;
-  if (template === 'jade') { template = 'oc-template-jade'; }
-  if (template === 'handlebars') { template = 'oc-template-handlebars'; }
+  if (template === 'jade') {
+    template = 'oc-template-jade';
+  }
+  if (template === 'handlebars') {
+    template = 'oc-template-handlebars';
+  }
+  if (requireOptions.compiler === true) {
+    template = template + '-compiler';
+  }
 
-  const localTemplate = path.join(__dirname, '../../', 'node_modules', template);
+  const localTemplate = path.join(
+    __dirname,
+    '../../',
+    'node_modules',
+    template
+  );
   const relativeTemplate = path.resolve('.', 'node_modules', template);
 
   try {
@@ -46,7 +64,7 @@ module.exports = function (template) {
     }
   }
 
-  if (!isValidTemplate(ocTemplate)) {
+  if (!isValidTemplate(ocTemplate, requireOptions)) {
     throw new Error(format(templateNotValid, template));
   }
 
