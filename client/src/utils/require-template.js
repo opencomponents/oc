d0c5279b3084c0ad4c51ab41c1bea57365e4352e
Nick Balestra <nbalestra@opentable.com>
2017-07-04 15:09:17 +0100

    require-template helper doesnt need to handle alternative paths, nore -compiler modules

--- client/src/utils/require-template.js
+++ client/src/utils/require-template.js
@@ -4,51 +4,45 @@ const format = require('stringformat');
 const path = require('path');
 
 const templateNotFound = 'Error requiring oc-template: "{0}" not found';
-const templateNotValid = 'Error requiring oc-template: "{0}" is not a valid oc-template';
+const templateNotValid =
+  'Error requiring oc-template: "{0}" is not a valid oc-template';
 
-function isValidTemplate(template, options){
+function isValidTemplate(template) {
   if (typeof template !== 'object') {
     return false;
   }
 
-  if (options.compiler === true) {
-    return typeof template.compile === 'function';
-  }
-
-  return [
-    'getInfo',
-    'getCompiledTemplate',
-    'render'
-  ].every((method) => typeof template[method] === 'function');
+  return ['getInfo', 'getCompiledTemplate', 'render'].every(
+    method => typeof template[method] === 'function'
+  );
 }
 
-module.exports = function(template, options) {
-  const requireOptions = options || {};
+module.exports = function(template) {
   let ocTemplate;
-  if (template === 'jade') { template = 'oc-template-jade'; }
-  if (template === 'handlebars') { template = 'oc-template-handlebars'; }
-  if (requireOptions.compiler === true) { template = template + '-compiler'; }
-
-  const localTemplate = path.join(__dirname, '../../../', 'node_modules', template);
-  const relativeTemplate = path.resolve('.', 'node_modules', template);
-
-  try {
-    if (require.cache && !!require.cache[localTemplate]) {
-      delete require.cache[localTemplate];
-    }
-    ocTemplate = require(localTemplate);
-  } catch(err) {
-    try {
-      if (require.cache && !!require.cache[relativeTemplate]) {
-        delete require.cache[relativeTemplate];
-      }
-      ocTemplate = require(relativeTemplate);
-    } catch (err) {
-      throw format(templateNotFound, template);
-    }
+  if (template === 'jade') {
+    template = 'oc-template-jade';
+  }
+  if (template === 'handlebars') {
+    template = 'oc-template-handlebars';
   }
 
-  if (!isValidTemplate(ocTemplate, requireOptions)) {
+  const templatePath = path.join(
+    __dirname,
+    '../../../',
+    'node_modules',
+    template
+  );
+
+  try {
+    if (require.cache && !!require.cache[templatePath]) {
+      delete require.cache[templatePath];
+    }
+    ocTemplate = require(templatePath);
+  } catch (err) {
+    throw format(templateNotFound, templatePath);
+  }
+
+  if (!isValidTemplate(ocTemplate)) {
     throw format(templateNotValid, template);
   }
 
