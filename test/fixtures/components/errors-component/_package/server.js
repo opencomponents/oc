6a1344c747c9139a88937fac32d63d82c277c81e
Nick Balestra <nbalestra@opentable.com>
2017-07-13 11:41:35 +0100

    removed unneded fixture

--- test/fixtures/components/errors-component/_package/server.js
+++
@@ -1,1 +0,0 @@
-"use strict";module.exports.data=function(r,e){"timeout"===r.params.errorType?setTimeout(function(){e(null,{error:!0,timeout:!0})},r.params.timeout||2e3):"500"===r.params.errorType?e("An error happened"):e(null,{error:!1})};
--- test/fixtures/components/errors-component/_package/template.js
+++
@@ -1,1 +0,0 @@
-var oc=oc||{};oc.components=oc.components||{},oc.components["884851aea70e65c9aed7f2f4f40e91566a24404c"]=function(o){var e,n=[],r=o||{};return function(o){n.push("<div>Any problems? "+jade.escape(null==(e=o?"Yep":"Nope")?"":e)+".  </div>")}.call(this,"error"in r?r.error:"undefined"!=typeof error?error:void 0),n.join("")};
