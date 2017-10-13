033f792072d652ca0e3489caa7f4e62c580d0188
Matteo Figus <matteofigus@gmail.com>
2017-10-13 14:40:14 +0100

    Moveed things aroound

---
+++ src/cli/domain/handle-dependencies/get-compiler.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const path = require('path');
+
+const cleanRequire = require('../../../utils/clean-require');
+const installCompiler = require('./install-compiler');
+
+module.exports = (options, cb) => {
+  const { compilerDep, componentPath, logger, pkg } = options;
+  const compilerPath = path.join(componentPath, 'node_modules', compilerDep);
+  const compiler = cleanRequire(compilerPath, { justTry: true });
+
+  if (compiler) {
+    return cb(null, compiler);
+  }
+
+  let dependency = compilerDep;
+  if (pkg.devDependencies[compilerDep]) {
+    dependency += `@${pkg.devDependencies[compilerDep]}`;
+  }
+
+  installCompiler(
+    {
+      compilerPath,
+      componentName: pkg.name,
+      componentPath,
+      dependency,
+      logger
+    },
+    cb
+  );
+};
--- src/cli/domain/handle-dependencies/index.js
+++ src/cli/domain/handle-dependencies/index.js
@@ -5,10 +5,9 @@ const fs = require('fs-extra');
 const path = require('path');
 const _ = require('lodash');
 
-const cleanRequire = require('../../../utils/clean-require');
 const ensureCompilerIsDeclaredAsDevDependency = require('./ensure-compiler-is-declared-as-devDependency');
+const getCompiler = require('./get-compiler');
 const getMissingDependencies = require('./get-missing-dependencies');
-const installCompiler = require('./install-compiler');
 const installMissingDependencies = require('./install-missing-dependencies');
 const isTemplateValid = require('../../../utils/is-template-valid');
 const strings = require('../../../resources');
@@ -41,7 +40,7 @@ module.exports = (options, callback) => {
               return next();
             }
 
-            cb(null, { componentPath, pkg, template });
+            cb(null, { componentPath, logger, pkg, template });
           },
 
           (options, cb) =>
@@ -50,35 +49,11 @@ module.exports = (options, callback) => {
               (err, compilerDep) => cb(err, _.extend(options, { compilerDep }))
             ),
 
-          (options, cb) => {
-            const { compilerDep, pkg } = options;
-            const compilerPath = path.join(
-              componentPath,
-              'node_modules',
-              compilerDep
-            );
-            const compiler = cleanRequire(compilerPath, { justTry: true });
+          (options, cb) =>
+            getCompiler(options, (err, compiler) =>
+              cb(err, _.extend(options, { compiler }))
+            ),
 
-            if (compiler) {
-              return cb(null, _.extend(options, { compiler }));
-            }
-
-            let dependency = compilerDep;
-            if (pkg.devDependencies[compilerDep]) {
-              dependency += `@${pkg.devDependencies[compilerDep]}`;
-            }
-
-            installCompiler(
-              {
-                compilerPath,
-                componentName: pkg.name,
-                componentPath,
-                dependency,
-                logger
-              },
-              (err, compiler) => cb(err, _.extend(options, { compiler }))
-            );
-          },
           (options, cb) => {
             const { compiler, pkg, template } = options;
             if (!compiler) {
