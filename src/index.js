e29d2a03f36757819dc69d2279846dfa54d74900
Nick Balestra <nbalestra@opentable.com>
2017-10-23 16:08:19 +0100

    expose cli to be used programmatically

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 'use strict';
 
+const _ = require('lodash');
+const Local = require('./cli/domain/local');
+
+function cliCommand(cmd, options, cb) {
+  const dependencies = {
+    local: new Local(),
+    logger: options.logger || { log() {} }
+  };
+  const opts = _.omit(options, 'logger');
+
+  require(`./cli/facade/${cmd}.js`)(dependencies)(opts, cb);
+}
+
+module.exports.cli = cliCommand;
 module.exports.Client = require('oc-client');
 module.exports.Registry = require('./registry');
