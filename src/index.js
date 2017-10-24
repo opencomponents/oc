aebd587098b8834a454346ea32d04ebcc9c64e3f
Nick Balestra <nbalestra@opentable.com>
2017-10-24 12:06:07 +0100

    moved dependencies to own file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const _ = require('lodash');
-const Local = require('./cli/domain/local');
-
 module.exports.cli = require('./cli/programmatic-api');
 module.exports.Client = require('oc-client');
 module.exports.Registry = require('./registry');
