4d851e2e4e2b6c02fd13ad2f7c11811ef5e54f65
Steven Bassett <stevenbassett@upwork.com>
2018-01-03 14:19:33 -0800

    addressed PR feedback

--- src/utils/error-to-string.js
+++ src/utils/error-to-string.js
@@ -1,7 +1,8 @@
+const _ = require('lodash');
+
 module.exports = function errorToString(err) {
-  const isString = typeof error === 'string';
-  const hasMessage = err != null && typeof err.msg === 'string';
-  if (isString) {
+  const hasMessage = err != null && _.isString(err.msg);
+  if (_.isString(err)) {
     return err;
   } else if (hasMessage) {
     return err.msg;
