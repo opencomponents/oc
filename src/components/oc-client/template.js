59b66d9e10924009e74fb0102b962d32d92aaee0
matteofigus <matteofigus@gmail.com>
2018-04-17 17:14:15 +0100

    oc-client using es6

---
+++ src/components/oc-client/template.js
@@ -0,0 +1,5 @@
+module.exports = ({ templates, staticPath }) =>
+  `<script>window.oc=window.oc||{};oc.conf=oc.conf||{};oc.conf.templates=(oc.conf.templates||[]).concat(${JSON.stringify(
+    templates
+  )});</script>
+<script src="${staticPath}src/oc-client.min.js" type="text/javascript"></script>`;
