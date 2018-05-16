00b73103e4857a38b4f233ac6c47f34412aca9f9
matteofigus <matteofigus@gmail.com>
2018-05-16 18:06:32 +0100

    UI bugfix

--- src/registry/views/partials/components-history.js
+++ src/registry/views/partials/components-history.js
@@ -1,6 +1,6 @@
 module.exports = vm => {
   const componentRow = ({ name, publishDate, version }) =>
-    `<a href=${name}/${version}/~info">
+    `<a href="${name}/${version}/~info">
   <div class="componentRow row table">
     <p class="release">${publishDate} - Published ${name}@${version}</p>
   </div>
