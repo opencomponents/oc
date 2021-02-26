ffa622515f508d44686127c2e2c5fc10c532e3e4
Daniel Tait <daniel.tait@skyscanner.net>
2021-02-26 17:22:51 +0000

    Handle scoped package when removing version
    This is done by:
    - Separating the path directory and base
    - Splitting the version on the base as before (The scope is in the dir string)
    - Returning the joined directory and name

--- src/utils/strip-version.js
+++ src/utils/strip-version.js
@@ -1,1 +1,6 @@
-module.exports = dependency => dependency.split('@')[0];
+const path = require('path');
+
+module.exports = dependency => {
+  const parts = path.parse(dependency);
+  return path.join(parts.dir, parts.base.split('@')[0]);
+};
--- test/unit/utils-strip-version.js
+++ test/unit/utils-strip-version.js
@@ -6,20 +6,43 @@ describe('utils : stripVersion', () => {
   const stripVersion = require('../../src/utils/strip-version');
 
   describe('when a non scoped dependency is provided', () => {
-    const dependency = '/path/to/dependency@1.0.0';
-    const name = stripVersion(dependency);
+    const dependency = '/path/to/dependency';
 
-    it('should return the dependency without the version', () => {
-      expect(name).to.equal('/path/to/dependency');
+    describe('including a version', () => {
+      const name = stripVersion(dependency + '@1.0.0');
+
+      it('should return the dependency without the version', () => {
+        expect(name).to.equal('/path/to/dependency');
+      });
+    });
+
+    describe('without a version', () => {
+      const name = stripVersion(dependency);
+
+      it('should return the unmodified dependency', () => {
+        expect(name).to.equal('/path/to/dependency');
+      });
     });
   });
 
   describe('when a scoped dependency is provided', () => {
-    const dependency = '/path/to/@the-scoped/package@1.2.3';
+    const dependency = '/path/to/@the-scoped/package';
     const name = stripVersion(dependency);
 
-    it('should return the dependency without the version', () => {
-      expect(name).to.equal('/path/to/@the-scoped/package');
+    describe('including a version', () => {
+      const name = stripVersion(dependency + '@1.2.3');
+
+      it('should return the dependency without the version', () => {
+        expect(name).to.equal('/path/to/@the-scoped/package');
+      });
+    });
+
+    describe('without a version', () => {
+      const name = stripVersion(dependency);
+
+      it('should return the unmodified dependency', () => {
+        expect(name).to.equal('/path/to/@the-scoped/package');
+      });
     });
   });
 });
