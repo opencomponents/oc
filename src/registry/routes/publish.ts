3df5f3ed40d382090146f9fa323de975b11f5a20
Ricardo Devis Agullo <mkredz@gmail.com>
2021-10-15 16:46:08 +0200

    enable noPropertyAccessFromIndexSignature option and fix problems

--- src/registry/routes/publish.ts
+++ src/registry/routes/publish.ts
@@ -6,7 +6,7 @@ import { Repository } from '../../types';
 
 export default function publish(repository: Repository) {
   return function (req: Request, res: Response): void {
-    if (!req.params.componentName || !req.params.componentVersion) {
+    if (!req.params['componentName'] || !req.params['componentVersion']) {
       res.errorDetails = 'malformed request';
       res.status(409).json({ error: res.errorDetails });
       return;
@@ -63,8 +63,8 @@ export default function publish(repository: Repository) {
 
       repository.publishComponent(
         pkgDetails,
-        req.params.componentName,
-        req.params.componentVersion,
+        req.params['componentName'],
+        req.params['componentVersion'],
         err => {
           if (err) {
             if (err.code === 'not_allowed') {
