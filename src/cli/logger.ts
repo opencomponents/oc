b565b64a86ec257b892ce61892804b0dd5b32872
Ricardo Devis Agullo <mkredz@gmail.com>
2021-10-16 14:10:36 +0200

    rename to keepline

--- src/cli/logger.ts
+++ src/cli/logger.ts
@@ -18,21 +18,21 @@ const logFns = {
   write: (msg: string) => process.stdout.write(msg.toString())
 };
 
-const log = (msg: string, color: Color | null, newLine: boolean) =>
-  logFns[newLine ? 'writeLn' : 'write'](color ? colors[color](msg) : msg);
+const log = (msg: string, color: Color | null, keepLine: boolean) =>
+  logFns[keepLine ? 'write' : 'writeLn'](color ? colors[color](msg) : msg);
 
 export interface Logger {
-  err: (msg: string, newLine?: boolean) => void;
-  log: (msg: string, newLine?: boolean) => void;
-  ok: (msg: string, newLine?: boolean) => void;
-  warn: (msg: string, newLine?: boolean) => void;
+  err: (msg: string, keepLine?: boolean) => void;
+  log: (msg: string, keepLine?: boolean) => void;
+  ok: (msg: string, keepLine?: boolean) => void;
+  warn: (msg: string, keepLine?: boolean) => void;
 }
 
 const logger: Logger = {
-  err: (msg: string, newLine = true) => log(msg, 'red', newLine),
-  log: (msg: string, newLine = true) => log(msg, null, newLine),
-  ok: (msg: string, newLine = true) => log(msg, 'green', newLine),
-  warn: (msg: string, newLine = true) => log(msg, 'yellow', newLine)
+  err: (msg: string, keepLine?: boolean) => log(msg, 'red', !!keepLine),
+  log: (msg: string, keepLine?: boolean) => log(msg, null, !!keepLine),
+  ok: (msg: string, keepLine?: boolean) => log(msg, 'green', !!keepLine),
+  warn: (msg: string, keepLine?: boolean) => log(msg, 'yellow', !!keepLine)
 };
 
 export default logger;
