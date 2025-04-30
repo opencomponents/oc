61d8b82f10589167d3ee1118672ea6691f2c2e92
Ricardo Devis Agullo <ricardo.agullo@theaccessgroup.com>
2025-04-30 10:11:55 +0200

    add crypto and url globals

--- src/registry/routes/helpers/get-component.ts
+++ src/registry/routes/helpers/get-component.ts
@@ -579,6 +579,9 @@ export default function getComponent(conf: Config, repository: Repository) {
                     Date,
                     Symbol,
                     eval: undefined,
+                    URL,
+                    URLSearchParams,
+                    crypto,
                     fetch: globalThis?.fetch
                   };
 
