8bade96eec94f6ba85c7ef93ad6cc71916a42322
Ricardo Devis Agullo <mkredz@gmail.com>
2025-07-25 12:46:54 +0200

    improve styles

--- src/registry/views/partials/components-history.tsx
+++ src/registry/views/partials/components-history.tsx
@@ -1,28 +1,15 @@
-import type { VM } from '../../../types';
-
-type ComponentHistory = VM['componentsHistory'];
-type ComponentHistoryItem = Exclude<ComponentHistory, undefined>[number];
-
-const ComponentsHistory = (props: { componentsHistory: ComponentHistory }) => {
-  const componentRow = ({
-    name,
-    publishDate,
-    version,
-    templateSize
-  }: ComponentHistoryItem) => (
-    <a href={`${name}/${version}/~info`}>
-      <div class="componentRow row table">
-        <p class="release">
-          {publishDate} - Published {name}@{version}
-          {templateSize ? ` [${Math.round(templateSize / 1024)} kb]` : ''}
-        </p>
-      </div>
-    </a>
-  );
-
+const ComponentsHistory = () => {
   return (
     <div id="components-history" class="box">
-      {props.componentsHistory ? props.componentsHistory.map(componentRow) : ''}
+      <div id="history-loader" class="loader">
+        <p>Loading components history...</p>
+      </div>
+      <div id="history-content" style="display: none;">
+        {/* Content will be populated by JavaScript */}
+      </div>
+      <div id="history-error" style="display: none;">
+        <p>Failed to load components history. Please try again later.</p>
+      </div>
     </div>
   );
 };
